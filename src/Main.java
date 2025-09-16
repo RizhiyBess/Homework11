75fda432a31f7aa428e570ab35f7b773b33ee605
RizhiyBess <m_m16@mail.ru>
2025-09-16 09:46:59 +0300

    Готовое ДЗ-11

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,49 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void checkYear(int year) {
+        if (year >= 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println(year + " год — високосный год");
+            } else {
+                System.out.println(year + " год — невисокосный год");
+            }
+        }
+    }
+
+    public static void inputDevice(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите стандартную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите стандартную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void inputDistance(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день");
+        } else if (deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня");
+        } else if (deliveryDistance < 100) {
+            System.out.println("Потребуется 3 дня");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        checkYear(1588);
+
+        System.out.println("Задание 2");
+        inputDevice(0, 2014);
+
+        System.out.println("Задание 3");
+        inputDistance(95);
     }
 }
