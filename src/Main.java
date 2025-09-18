0a17e684c24cfce2f0336c661412b15432b2f370
RizhiyBess <m_m16@mail.ru>
2025-09-18 19:08:37 +0300

    Исправленное ДЗ-11

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,55 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void checkYear(int year) {
-        if (year >= 1584) {
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                if ((year >= 1584 && year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
                 System.out.println(year + " год — високосный год");
             } else {
                 System.out.println(year + " год — невисокосный год");
             }
-        }
     }
 
-    public static void inputDevice(int clientOS, int clientDeviceYear) {
-        if (clientOS == 0 && clientDeviceYear < 2015) {
+    public static void printAppVersionRecommendation(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 0 && clientDeviceYear >= currentYear) {
             System.out.println("Установите стандартную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите стандартную версию приложения для Android по ссылке");
         }
     }
 
-    public static void inputDistance(int deliveryDistance) {
+    public static int calculatingDeliveryTime(int deliveryDistance) {
         if (deliveryDistance < 20) {
-            System.out.println("Потребуется 1 день");
+            return 1;
         } else if (deliveryDistance < 60) {
-            System.out.println("Потребуется 2 дня");
+            return 2;
         } else if (deliveryDistance < 100) {
-            System.out.println("Потребуется 3 дня");
+           return 3;
         } else {
-            System.out.println("Доставки нет");
+            return 0;
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        checkYear(1588);
+        checkYear(1587);
 
         System.out.println("Задание 2");
-        inputDevice(0, 2014);
+        printAppVersionRecommendation(0, 2014);
 
         System.out.println("Задание 3");
-        inputDistance(95);
+        int days = calculatingDeliveryTime(50);
+        if (days > 0) {
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Доставка не возможна");
+        }
     }
 }
